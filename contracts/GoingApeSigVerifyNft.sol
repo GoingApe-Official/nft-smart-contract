a3858c26b96bba32cc5d370df4a3d9867b9367ca
Cloudgen Wong <cloudgen@riodefi.com>
2022-03-09 15:35:52 +0800

    updated smart contract

--- contracts/GoingApeSigVerifyNft.sol
+++ contracts/GoingApeSigVerifyNft.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.12;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "erc721a/contracts/ERC721A.sol";
@@ -30,7 +30,7 @@ contract GoingApeSigVerifyNft is ERC721A, Ownable, ReentrancyGuard {
 
     uint256 public immutable maxSupply = 10000;
     uint256 public immutable privateSaleSupply = 1000;
-    uint256 public immutable reserveSupply = 1000;
+    uint256 public immutable reserveSupply = 3027;
 
     uint256 public tokensReserved;
     uint256 public privateSales;
@@ -42,10 +42,14 @@ contract GoingApeSigVerifyNft is ERC721A, Ownable, ReentrancyGuard {
     mapping(address => uint256) public publicSaleMints;
 
     uint256 public constant MAX_WHITELIST_MINT = 1;
-    uint256 public constant MAX_PRIVATE_MINT = 5;
-    uint256 public constant MAX_PUBLIC_MINT = 3;
+    uint256 public constant MAX_PRIVATE_MINT = 10;
+    uint256 public constant MAX_PUBLIC_MINT = 5;
 
     string public PROVENANCE = "";
+
+    event PublicCostChanged(uint256 newCost);
+    event WhitelistSaleCostChanged(uint256 newCost);
+    event PrivateSaleCostChanged(uint256 newCost);
 
     constructor(
         string memory _name,
@@ -61,7 +65,7 @@ contract GoingApeSigVerifyNft is ERC721A, Ownable, ReentrancyGuard {
     }
 
     // public
-    function publicMint(uint256 _mintAmount) public payable nonReentrant {
+    function publicMint(uint256 _mintAmount) external payable nonReentrant {
         uint256 supply = totalSupply();
         require(publicSaleIsActive, "public sale not open");
         require(tx.origin == msg.sender, "contract is not allowed to mint");
@@ -74,16 +78,20 @@ contract GoingApeSigVerifyNft is ERC721A, Ownable, ReentrancyGuard {
             _mintAmount + publicSaleMints[msg.sender] <= MAX_PUBLIC_MINT,
             "public mint amount per wallet exceeded"
         );
-        require(
-            msg.value == publicSaleCost * _mintAmount,
-            "incorrect purchase amount"
-        );
-        _safeMint(msg.sender, _mintAmount);
+        refundIfOver(publicSaleCost * _mintAmount);
         publicSaleMints[msg.sender] += _mintAmount;
+        _safeMint(msg.sender, _mintAmount);
+    }
+
+    function refundIfOver(uint256 price) private {
+        require(msg.value >= price, "Need to send more ETH");
+        if (msg.value > price) {
+            payable(msg.sender).transfer(msg.value - price);
+        }
     }
 
     function whitelistSaleMint(uint256 _mintAmount, bytes memory _signature)
-        public
+        external
         payable
         nonReentrant
     {
@@ -99,20 +107,17 @@ contract GoingApeSigVerifyNft is ERC721A, Ownable, ReentrancyGuard {
             _mintAmount + whitelistSaleMints[msg.sender] <= MAX_WHITELIST_MINT,
             "whitelist mint amount per wallet exceeded"
         );
-        require(
-            msg.value == whitelistSaleCost * _mintAmount,
-            "incorrect purchase amount"
-        );
 
         address signerOwner = signatureWallet(msg.sender, 1, _signature);
         require(signerOwner == signer, "Not authorized to mint");
+        refundIfOver(whitelistSaleCost * _mintAmount);
 
-        _safeMint(msg.sender, _mintAmount);
         whitelistSaleMints[msg.sender] += _mintAmount;
+        _safeMint(msg.sender, _mintAmount);
     }
 
     function privateSaleMint(uint256 _mintAmount, bytes memory _signature)
-        public
+        external
         payable
         nonReentrant
     {
@@ -132,17 +137,14 @@ contract GoingApeSigVerifyNft is ERC721A, Ownable, ReentrancyGuard {
             _mintAmount + privateSaleMints[msg.sender] <= MAX_PRIVATE_MINT,
             "private sale mint amount per wallet exceeded"
         );
-        require(
-            msg.value == privateSaleCost * _mintAmount,
-            "incorrect purchase amount"
-        );
 
         address signerOwner = signatureWallet(msg.sender, 0, _signature);
         require(signerOwner == signer, "Not authorized to mint");
-
-        _safeMint(msg.sender, _mintAmount);
+        refundIfOver(privateSaleCost * _mintAmount);
         privateSaleMints[msg.sender] += _mintAmount;
         privateSales += _mintAmount;
+
+        _safeMint(msg.sender, _mintAmount);
     }
 
     function signatureWallet(
@@ -157,7 +159,7 @@ contract GoingApeSigVerifyNft is ERC721A, Ownable, ReentrancyGuard {
             );
     }
 
-    function numberMinted(address owner) public view returns (uint256) {
+    function numberMinted(address owner) external view returns (uint256) {
         return _numberMinted(owner);
     }
 
@@ -170,43 +172,49 @@ contract GoingApeSigVerifyNft is ERC721A, Ownable, ReentrancyGuard {
     }
 
     //only owner
-    function setPublicCost(uint256 _newCost) public onlyOwner {
+    function setPublicCost(uint256 _newCost) external onlyOwner {
         publicSaleCost = _newCost;
+        emit PublicCostChanged(_newCost);
     }
 
-    function setWhitelistSaleCost(uint256 _newCost) public onlyOwner {
+    function setWhitelistSaleCost(uint256 _newCost) external onlyOwner {
         whitelistSaleCost = _newCost;
+        emit WhitelistSaleCostChanged(_newCost);
     }
 
-    function setPrivateSaleCost(uint256 _newCost) public onlyOwner {
+    function setPrivateSaleCost(uint256 _newCost) external onlyOwner {
         privateSaleCost = _newCost;
+        emit PrivateSaleCostChanged(_newCost);
     }
 
-    function setProvenanceHash(string memory _provenanceHash) public onlyOwner {
+    function setProvenanceHash(string memory _provenanceHash)
+        external
+        onlyOwner
+    {
         PROVENANCE = _provenanceHash;
     }
 
-    function setBaseURI(string memory _newBaseURI) public onlyOwner {
+    function setBaseURI(string memory _newBaseURI) external onlyOwner {
         baseURI = _newBaseURI;
     }
 
-    function setPublicSaleState(bool _newState) public onlyOwner {
+    function setPublicSaleState(bool _newState) external onlyOwner {
         publicSaleIsActive = _newState;
     }
 
-    function setWhitelistSaleState(bool _newState) public onlyOwner {
+    function setWhitelistSaleState(bool _newState) external onlyOwner {
         whitelistSaleisActive = _newState;
     }
 
-    function setPrivateSaleState(bool _newState) public onlyOwner {
+    function setPrivateSaleState(bool _newState) external onlyOwner {
         privateSaleisActive = _newState;
     }
 
-    function setSigner(address _newSigner) public onlyOwner {
+    function setSigner(address _newSigner) external onlyOwner {
         signer = _newSigner;
     }
 
-    function reserve(uint256 _n, address _receiver) public onlyOwner {
+    function reserve(uint256 _n, address _receiver) external onlyOwner {
         uint256 supply = totalSupply();
         require(_receiver != address(0), "not zero address");
         require(_n > 0, "not zero amount");
@@ -215,12 +223,12 @@ contract GoingApeSigVerifyNft is ERC721A, Ownable, ReentrancyGuard {
             tokensReserved + _n <= reserveSupply,
             "max reserve amount exceeded"
         );
-        _safeMint(_receiver, _n);
         tokensReserved += _n;
+        _safeMint(_receiver, _n);
     }
 
-    function withdrawBalance(uint256 _amount) public onlyOwner {
-        require(_amount <= address(this).balance);
+    function withdrawBalance(uint256 _amount) external onlyOwner {
+        require(_amount <= address(this).balance, "withdraw amount too large");
         payable(msg.sender).transfer(_amount);
     }
 }
